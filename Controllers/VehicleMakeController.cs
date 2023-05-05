01d7e626dd3011548721ca17865facd8113fe47d
akruhoberec1 <akruhoberec1@outlook.com>
2023-05-05 14:24:22 +0200

    Model and Controller
    - Creating first model and controller
    - Returning data using Status codes

---
+++ Controllers/VehicleMakeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_BestPractices.Models;
+
+namespace WebApi_BestPractices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleMakeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllMakes()
+        {
+            var makes = new List<VehicleMake>
+            {
+                new VehicleMake{ Id = 1, Name = "Volkswagen", Abrv = "VW"}
+            };
+        }
+    }
+}
---
+++ Models/VehicleMake.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi_BestPractices.Models
+{
+    public class VehicleMake
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Abrv { get; set; } = string.Empty;
+
+    }
+}

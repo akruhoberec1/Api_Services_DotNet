14aae8b72db9cb0458bdb172586befd6f1a248fc
akruhoberec1 <akruhoberec1@outlook.com>
2023-05-05 14:43:13 +0200

    Controllers

--- Controllers/VehicleMakeController.cs
+++ Controllers/VehicleMakeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 using WebApi_BestPractices.Models;
 
 namespace WebApi_BestPractices.Controllers
@@ -8,13 +9,33 @@ namespace WebApi_BestPractices.Controllers
     [ApiController]
     public class VehicleMakeController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> GetAllMakes()
-        {
-            var makes = new List<VehicleMake>
+        private static List<VehicleMake> makes = new List<VehicleMake>
             {
-                new VehicleMake{ Id = 1, Name = "Volkswagen", Abrv = "VW"}
+                new VehicleMake
+                { 
+                    Id = 1, 
+                    Name = "Volkswagen", 
+                    Abrv = "VW"
+                },
+                new VehicleMake
+                {
+                    Id = 2,
+                    Name = "Bayerische Motoren Werke",
+                    Abrv = "BMW"
+                },
             };
+        [HttpGet]
+        public async Task<ActionResult<List<VehicleMake>>> GetAllMakes()
+        {
+            return Ok(makes);
         }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<VehicleMake>>> GetSingleMake(int id)
+        {
+            var make = makes.Find(x => x.Id == id);
+            return Ok(make);
+        }
+
     }
 }
